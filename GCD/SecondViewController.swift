4cdebe5f90d951b511c74b4173d9d3c791486492
Paul <paulrommel87@gmail.com>
2022-02-06 10:07:42 +0300

    6 Интерфейс приложения

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Павел Попов on 06.02.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+    
+    }
+}
